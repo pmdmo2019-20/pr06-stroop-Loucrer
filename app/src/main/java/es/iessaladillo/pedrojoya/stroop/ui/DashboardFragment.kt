bebafe9634ad79cc9ed8382349bd0c778a7d69fb
Jose Luis <jldrm1996@gmail.com>
2020-02-28 14:18:09 +0100

    Avance proyecto

---
+++ app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/DashboardFragment.kt
@@ -0,0 +1,8 @@
+package es.iessaladillo.pedrojoya.stroop.ui
+
+import androidx.fragment.app.Fragment
+import es.iessaladillo.pedrojoya.stroop.R
+
+class DashboardFragment : Fragment(R.layout.dashboard_fragment){
+
+}
--- app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/stroop/ui/MainActivity.kt
@@ -1,10 +1,31 @@
 package es.iessaladillo.pedrojoya.stroop.ui
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import es.iessaladillo.pedrojoya.stroop.R
 
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO
+    // Obtenemos el grafo de navegacion:
+    private val navController: NavController by lazy {
+        findNavController(R.id.navHostFragment) //Este es el id de nuestro grafo en nuestra actividad
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        setupViews()
+    }
+
+    private fun setupViews() {
+
+    }
+
 
 }
